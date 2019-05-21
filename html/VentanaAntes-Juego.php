2b2c6eaf202f353399f48e616790be708d306a42
EstebanCarranza <esteban.carranza@outlook.com>
2019-05-20 20:57:17 -0500

    Revert "Particulas y cambio de modelos"
    This reverts commit e69eeaee226d4501600502fd1300d0f7b8e1fff7.

---
+++ html/VentanaAntes-Juego.php
@@ -0,0 +1,73 @@
+<!doctype html>
+<html lang="en">
+    <head>
+        <title> Juego </title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css?family=Roboto:100,600" rel="stylesheet" type="text/css">
+        <link rel="shortcut icon" href="../images/favicon.ico"/>
+        <!-- JS -->
+        <script type="text/javascript" src="../js/bootstrap.js"></script>
+        <!-- Styles -->
+        
+        <link rel="stylesheet" type="text/css" href="../css/bootstrap.css">
+
+    </head>
+
+    <body class="text-center">
+
+        <!--Navbar-->
+    <div class="container-fluid d-flex w-100 h-100 p-3 mx-auto flex-column">
+    <?php include('navbar.php');?>
+
+ <!-- Large modal -->
+
+ <div class="modalPausa" style="top:12%;">
+     <div class="justify-content-center">
+     <button type="button" class="close" data-dismiss="modal1" style="color:white;" aria-label="Close">X</button><br>
+          <h2 style="color:white;">INSTRUCCIONES</h2><br>
+          <h6 style="color:white;">Recolecta la mayor cantidad de gemas posibles llevándolas a tu base para ganar a tu contrincante.</h6><br>
+          <h5 style="color:white;">El jugador 1 deberá utilizar las teclas ASDW para moverse, mientras el jugador 2 deberá utilizar JKLI</h5><br>
+          <img src="../images/teclas.png"><br><br>
+          <button class="BtnBegin"><h3>Empezar</h3></button><br><br>
+    </div>
+</div>
+
+ <!-- <button onclick="myFunction()">Try it</button>
+    <script>
+        function myFunction() {
+          var x = document.getElementById("Intento");
+          if (x.style.display === "none") {
+            x.style.display = "block";
+          } else {
+            x.style.display = "none";
+          }
+        }
+</script>-->
+
+
+ <!--Intento Pop up-->
+
+
+        <!--Contenido-->
+        <div class="card text-white bg-primary">     
+                <br><br>    
+            <main role="main" class="inner cover">
+            </main>
+            <br><br> <br><br> <br>  
+          
+        </div>
+    </div>
+
+        <!--FOOTER-->
+   <div class="container-fluid">
+        <footer>
+            <table class="navbar navbar-expand-lg navbar-light bg-light" style="width:100%">  
+                <td><p class="ArrobaHispanofic" style="color:white;">© Crystals collecters 2019</p></td>
+            </table>
+        </footer>
+    </div>
+
+        </body>
+</html>
---
+++ html/VentanaPausa.php
@@ -0,0 +1,62 @@
+<!doctype html>
+<html lang="en">
+    <head>
+        <title> Pausa </title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css?family=Roboto:100,600" rel="stylesheet" type="text/css">
+        <link rel="shortcut icon" href="../images/favicon.ico"/>
+        <!-- JS -->
+        <script type="text/javascript" src="../js/bootstrap.js"></script>
+        <!-- Styles -->
+        
+        <link rel="stylesheet" type="text/css" href="../css/bootstrap.css">
+
+    </head>
+
+    <body class="text-center">
+
+        <!--Navbar-->
+    <div class="container-fluid d-flex w-100 h-100 p-3 mx-auto flex-column">
+    <?php include('navbar.php');?>
+
+ <!-- Large modal -->
+
+ <div class="modalPausa">
+     <div class="justify-content-center">
+     <button type="button" class="close" data-dismiss="modal1" style="color:white;" aria-label="Close">X</button><br>
+          <h1 style="color:white;">PAUSA</h1>
+          <br>
+          <button class="BtnOpcion"><h4>Continuar partida</h4></button><br><br>
+          <button class="BtnOpcion"><h4>Reiniciar nivel</h4></button><br><br>
+          <button class="BtnOpcion"><h4>Opciones de sonido</h4></button><br><br>
+          <button class="BtnOpcion"><h4>Salir del juego</h4></button><br><br><br><br>
+    </div>
+</div>
+
+ <!--Intento Pop up-->
+
+
+        <!--Contenido-->
+        <div class="card text-white bg-primary">     
+                <br><br>    
+                <main role="main" class="inner cover">
+                        <img src="../images/Juego.png" width="1100" height="500">
+                    </main>
+            <br><br> <br><br> <br>  
+          
+        </div>
+    </div>
+
+        <!--FOOTER-->
+   <div class="container-fluid">
+        <footer>
+            <table class="navbar navbar-expand-lg navbar-light bg-light" style="width:100%">  
+                <td><p class="ArrobaHispanofic" style="color:white;">© Crystals collecters 2019</p></td>
+            </table>
+        </footer>
+    </div>
+
+        </body>
+</html>
